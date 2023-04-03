45db1fc87497a6a1545c1a58c80c5599505a6ce8
Nazariy90 <pinhvin123@gmail.com>
2023-04-03 19:53:13 +0300

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
-console.log('ddsf');
+const startBTN = document.querySelector('button[data-start]');
+const stopBTN = document.querySelector('button[data-stop]');
+const bodyBackgroundColor = document.body;
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function changeColorFoo() {
+  const currentColor = getRandomHexColor();
+  bodyBackgroundColor.style.backgroundColor = currentColor;
+}
+
+startBTN.addEventListener('click', () => {
+  timerId = setInterval(changeColorFoo, 1000);
+  startBTN.disabled = true;
+});
+
+stopBTN.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBTN.disabled = false;
+});
